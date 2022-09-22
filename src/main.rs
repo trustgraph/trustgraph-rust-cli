088ba588bb38c7351b74e62578b70b849fd78742
Golda Velez <golda@3box.io>
2022-09-22 14:43:24 -0700

    add some thoughts on what to do next

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,24 @@ use clap::Parser;
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
+
+
+/* TODO next:
+
+  decide on some initial use cases
+
+  make ArgGroup
+    make prompts that enforce things
+
+  do something with the args - call a API?  write to trustgraph right now?
+  what will be the interface between the CLI and the backend?  is there a backend?
+
+  spit out jsonld and optionally pipe to a thing that writes it to various storages or pushes it out
+
+  make separate components for cli, and pipes
+
+*/
+
 struct Args {
    /// Name of the person to greet
    #[clap(short, long, value_parser)]
